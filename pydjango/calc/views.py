f23235542c8c4b92970239cc35961a43041db6ed
iagam <iamagam03@gmail.com>
2022-10-05 02:13:22 +0530

    Dynamic content test

--- pydjango/calc/views.py
+++ pydjango/calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, "home.html", {"name": "Agam"})
