42fab00020c4d98221696adf3f018027438877c9
iagam <iamagam03@gmail.com>
2022-10-05 17:03:00 +0530

    Create a form to add two numbers

--- pydjango/calc/urls.py
+++ pydjango/calc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("add", views.add, name="add"),
 ]
--- pydjango/calc/views.py
+++ pydjango/calc/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html", {"name": "Agam"})
+
+
+def add(request):
+
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+
+    sum = val1 + val2
+
+    return render(request, "result.html", {"result": sum})
