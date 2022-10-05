bc60517ddd76d84eb62c601821543fba9b3eaaea
iagam <iamagam03@gmail.com>
2022-10-06 00:43:25 +0530

    Remove nav links

--- pydjango/fabrica/views.py
+++ pydjango/fabrica/views.py
@@ -4,7 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
-
-
-def products(request):
-    return render(request, "products.html")
