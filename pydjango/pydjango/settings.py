0b3a7760f8d62e388601e3e06f014105dad6c500
iagam <iamagam03@gmail.com>
2022-10-07 02:33:37 +0530

    Add app to installed apps

--- pydjango/pydjango/settings.py
+++ pydjango/pydjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "fabrica.apps.FabricaConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
