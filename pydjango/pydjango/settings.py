cc58da30a4e2f43a222fd6012952a48b0fa0840f
iagam <iamagam03@gmail.com>
2022-10-07 02:11:59 +0530

    Add postgres db backend

--- pydjango/pydjango/settings.py
+++ pydjango/pydjango/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = "pydjango.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "braintree",
+        "USER": "postgres",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
     }
 }
 
