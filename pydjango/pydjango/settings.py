fc5891083221de51857eb2bb2beb42ad3ac9e073
iagam <iamagam03@gmail.com>
2022-10-08 16:58:01 +0530

    Final commit

--- pydjango/pydjango/settings.py
+++ pydjango/pydjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%%tt6(ycz5((iextb2kech)swhr427o!i91)^m+nzu%exb8asv"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
